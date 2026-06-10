5b73fe969fde167ab5182c02ef9f28d52c792db0
LavenderEdit <gercermagden@gmail.com>
2026-06-09 19:21:16 -0500

    feat(js): Simplify traduccionTipo export by removing unnecessary function definition

--- JS/traductor.js
+++ JS/traductor.js
@@ -1,11 +1,3 @@
-import { tiposPokemon } from "./tipos.js";
+import { traducirTipo } from "./tipos.js";
 
-export function traducirTipo(tipo, idioma = "es") {
-  const clave = tipo?.toLowerCase?.();
-  const tipoTraducido = clave ? tiposPokemon[clave] : null;
-
-  if (tipoTraducido) {
-    return tipoTraducido[idioma] || tipoTraducido["es"];
-  }
-  return tipo;
-}
+export { traducirTipo };
